a9e52d800598fb704f839ce6138579351b12dfb0
Arturo Nereu <hdgam3r@gmail.com>
2019-10-10 19:55:17 -0500

    Added first shader and building blocks for project

---
+++ Assets/Scripts/ShaderSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    Material m_CanvasObjectMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_CanvasObjectMaterial = GetComponent<Renderer>().material;
+
+        //SwitchToShaderGraph("Shader Graphs/01_Justice");
+    }
+
+    void SwitchToShader(string shaderName)
+    {
+
+    }
+
+    void SwitchToShaderGraph(string shadergraphName)
+    {
+        m_CanvasObjectMaterial.shader = Shader.Find(shadergraphName);
+    }
+}
